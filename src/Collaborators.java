08cb9521c7c55d8503a9fdbe0063717597682b82
xaskoiking <kraaghavanster@gmail.com>
2019-06-17 18:59:30 -0400

    First Commit

---
+++ src/Collaborators.java
@@ -0,0 +1,10 @@
+
+public class Collaborators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello MCIT Week 4. The collaborator will add a new message after this ");
+	}
+
+}
