d164520c54371719141feb994bf4a7174e451a50
xaskoiking <kraaghavanster@gmail.com>
2019-06-18 07:21:10 -0400

    Third Commit

--- src/Collaborators.java
+++ src/Collaborators.java
@@ -4,7 +4,8 @@ public class Collaborators {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello MCIT Week 4. CN has made an edit on this line - 6/17/2019 ");
+		System.out.println("Hello MCIT Week 4. The collaborator will add a new message after this ");
+		System.out.println("Step2: New line after collaborator's commit. This will be used to rebase and pull");
 	}
 
 }
