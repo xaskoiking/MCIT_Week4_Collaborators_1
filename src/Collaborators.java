6e1c84fbc1fc6c3fd5302440beb0a4749b4e5b79
xaskoiking <kraaghavanster@gmail.com>
2019-06-18 19:18:22 -0400

    Same Line Commit

--- src/Collaborators.java
+++ src/Collaborators.java
@@ -2,7 +2,8 @@
 public class Collaborators {
 
 	public static void main(String[] args) {
-			System.out.println("Hello MCIT Week 4. CN has made an edit on this line - 6/17/2019");
-		}
+			System.out.println("Hello MCIT Week 4. CN has made an edit on this line - 6/17/2019. Updated Commit by Raag");
+
+	}
 
 }
