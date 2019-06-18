311516dcfe2e4e89fac3bfce7d3fddc70d06f522
xaskoiking <kraaghavanster@gmail.com>
2019-06-18 07:30:00 -0400

    Commiting the changes

--- src/Collaborators.java
+++ src/Collaborators.java
@@ -2,10 +2,8 @@
 public class Collaborators {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		System.out.println("Hello MCIT Week 4. The collaborator will add a new message after this ");
-		System.out.println("Step2: New line after collaborator's commit. This will be used to rebase and pull");
-	}
+			System.out.println("Step 0: User 1 Adding the comment");
+			//TODO the Collaborator will add a new change in the file and add a new commit below
+		}
 
 }
