e8f42194ba744e1b9b3112aca81e49751dc6332e
cnguyen <cnguyenc@seas.upenn.edu>
2019-06-17 22:25:08 -1000

    Modified Collaborators.java println

--- src/Collaborators.java
+++ src/Collaborators.java
@@ -4,7 +4,7 @@ public class Collaborators {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello MCIT Week 4. The collaborator will add a new message after this ");
+		System.out.println("Hello MCIT Week 4. CN has made an edit on this line - 6/17/2019 ");
 	}
 
 }
