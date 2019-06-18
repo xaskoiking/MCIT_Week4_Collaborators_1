e2885395b77691893ea764e3e11e62ad69bd46a8
xaskoiking <kraaghavanster@gmail.com>
2019-06-18 07:36:16 -0400

    Fifth commit

--- src/Collaborators.java
+++ src/Collaborators.java
@@ -2,8 +2,7 @@
 public class Collaborators {
 
 	public static void main(String[] args) {
-			System.out.println("Step 0: User 1 Adding the comment");
-			//TODO the Collaborator will add a new change in the file and add a new commit below
+			System.out.println("Hello MCIT Week 4. CN has made an edit on this line - 6/17/2019");
 		}
 
 }
